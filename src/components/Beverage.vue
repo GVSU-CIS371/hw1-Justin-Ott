8a601e0b0837b6bb1158b4848d5d6bd138afc23d
Justin-Ott <ottj@mail.gvsu.edu>
2025-03-19 18:40:56 -0400

    Moved files from personal repo into proper hw repo

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -1,20 +1,29 @@
 <template>
   <Mug>
+    <!-- Conditionally render Hot or Cold -->
     <Cold v-if="isIced" />
     <Hot v-else />
+
+    <!-- Contents with Conditional Rendering -->
     <Contents>
+      <!-- Creamer (Top Slot) -->
       <template v-slot:top>
-        <Creamer />
+        <Creamer v-if="currentCreamer !== 'transparent'" />
       </template>
+
+      <!-- Syrup (Mid Slot) -->
       <template v-slot:mid>
-        <Syrup />
+        <Syrup v-if="currentSyrup !== 'transparent'" />
       </template>
+
+      <!-- Base Beverage (Bottom Slot) -->
       <template v-slot:bottom>
         <Base />
       </template>
     </Contents>
   </Mug>
 </template>
+
 <script setup lang="ts">
 import Contents from "./Contents.vue";
 import Mug from "./Mug.vue";
@@ -23,6 +32,7 @@ import Base from "./Base.vue";
 import Creamer from "./Creamer.vue";
 import Hot from "./Hot.vue";
 import Cold from "./Cold.vue";
+import { currentCreamer, currentSyrup } from "../stores/beverage"; // Import reactive state
 
 type Props = {
   isIced: boolean;
--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="syrup"></div>
+  <div 
+    class="syrup" 
+    :style="{ backgroundColor: currentSyrup === 'transparent' ? 'transparent' : currentSyrup }"
+  ></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { currentSyrup } from "../stores/beverage";
+</script>
+
 <style lang="scss" scoped>
 .syrup {
   transform: translateY(400%);
-  background-color: #c6c6c6;
   position: relative;
   width: 100%;
-  height: 20%;
+  height: 40%;
+  z-index: 150;
   animation: pour-tea 2s 1s forwards;
-  z-index: 2;
 }
 </style>
